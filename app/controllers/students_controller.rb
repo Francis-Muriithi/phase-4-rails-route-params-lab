c0713c6cd6697725a3a5cb314c08aafbec9cd91d
Francis-Muriithi <mufraggie@gmail.com>
2022-11-21 23:27:23 +0300

    assignment done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,20 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+
+  def first_student
+    student1 = Student.first
+    render json:student1
+  end
+
+  def second_student
+    student2 = Student.second
+    render json:student2
+  end
+
+  def show 
+    student = Student.find(params[:id])
+    render json:student
+  end
+
 end
